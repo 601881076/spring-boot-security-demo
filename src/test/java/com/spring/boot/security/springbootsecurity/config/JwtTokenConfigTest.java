741b134917e4e91b02857a436d6e9e3a8951bded
601881076 <601881076@qq.com>
2021-12-14 22:00:36 +0800

    JwtToken解析 JwtToken塞入claime

--- src/test/java/com/spring/boot/security/springbootsecurity/config/JwtTokenConfigTest.java
+++ src/test/java/com/spring/boot/security/springbootsecurity/config/JwtTokenConfigTest.java
@@ -1,13 +1,14 @@
 package com.spring.boot.security.springbootsecurity.config;
 
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.impl.Base64Codec;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
 * @Description:    jjwtToken获取测试
@@ -23,8 +24,14 @@ import java.util.Date;
 @SpringBootTest
 public class JwtTokenConfigTest {
 
+    /**
+     * 创建Token
+     */
     @Test
     public void getJwtToken() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("sex","男");
+        map.put("age",26);
         JwtBuilder jwtBuilder = Jwts.builder()
                 // 创建日期
                 .setIssuedAt(new Date())
@@ -32,6 +39,8 @@ public class JwtTokenConfigTest {
                 .setSubject("tanyi")
                 // 声明的标识
                 .setId("12345")
+                // 使用setClaims之后，主体、创建日期、标识都不会保存再body了
+                .setClaims(map)
                 // 设置签名手段，参数1：算法， 参数2： 盐
                 .signWith(SignatureAlgorithm.HS256, "secret");
 
@@ -48,4 +57,85 @@ public class JwtTokenConfigTest {
         System.out.println(Base64Codec.BASE64.decodeToString(jwtTokenArray[2]));
 
     }
+
+    /**
+     * 解析Token
+     */
+    @Test
+    public void parseJwtToken() {
+
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzZXgiOiLnlLciLCJhZ2UiOjI2fQ.cCp6gAIxV0_nfy3uPtfKTvtR23MsFdY6_rFlP0IxYiI";
+
+        // 解析
+        Jws<Claims> secret = Jwts.parser()
+                // 放入盐key
+                .setSigningKey("secret")
+                // 解析token
+                .parseClaimsJws(token);
+
+        System.out.println(secret);
+    }
+
+    /**
+     * 创建token -- 包含过期时间
+     */
+    @Test
+    public void createJwtTokenHasExp() {
+        Map<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sex","男");
+        hashMap.put("age",26);
+        hashMap.put("date",new Date());
+
+        long nowDate = System.currentTimeMillis();
+        // 一分钟后到期
+        long expiration = nowDate + 60 * 1000;
+
+        JwtBuilder secret = Jwts.builder()
+                .setClaims(hashMap)
+                // 设置过期时间
+                .setExpiration(new Date(expiration))
+                .signWith(SignatureAlgorithm.HS256, "secret");
+
+
+        String token = secret.compact();
+
+        System.out.println(token);
+    }
+
+    /**
+     * 解析token
+     */
+    @Test
+    public void parseTokenHasExpiration() {
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoxNjM5NDkwMTQ0MDY4LCJzZXgiOiLnlLciLCJleHAiOjE2Mzk0OTAyMDQsImFnZSI6MjZ9.sQm-LiOmYtVe-r9KZ1BloRlb4h42rNjoCDUoS4atpO0";
+
+        Jws<Claims> secret = Jwts.parser()
+                .setSigningKey("secret")
+                .parseClaimsJws(token);
+
+        // 获取到期时间
+        Claims body = secret.getBody();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+
+        System.out.println(secret);
+        System.out.println("到期时间 = " + sdf.format(body.getExpiration()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
